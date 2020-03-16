f4ce639db9d54a994fb45df4a446002a58c1f9c2
AliMehrabani <ali1379m@gmail.com>
2020-03-16 20:04:55 +0330

    add method defined

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("git Workshop");
+        System.out.println("git Workshop");
+    }
+
+    static int add(int a, int b) {
+        int c = a + b;
+        return c;
     }
 }
